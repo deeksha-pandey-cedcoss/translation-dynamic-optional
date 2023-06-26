89da2fef78349789edba093802ec027711788a12
deeksha01032023 <deekshapandey@cedcoss.com>
2023-06-26 12:55:55 +0530

    done

---
+++ src/app/locals/Locale.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace MyApp;
+
+use Phalcon\Di\Injectable;
+use Phalcon\Translate\Adapter\NativeArray;
+use Phalcon\Translate\InterpolatorFactory;
+use Phalcon\Translate\TranslateFactory;
+
+class Locale extends Injectable
+{
+    /**
+     * @return NativeArray
+     */
+    public function getTranslator(): NativeArray
+    {
+
+
+        $language = $this->cookies->get("lang");
+
+        $messages = [];
+
+        $translationFile = APP_PATH . '/messages/' . $language;
+
+        if (true !== file_exists($translationFile)) {
+            $translationFile = APP_PATH . '/messages/en-GB.php';
+        }
+        require $translationFile;
+
+        $interpolator = new InterpolatorFactory();
+        $factory      = new TranslateFactory($interpolator);
+
+
+        return $factory->newInstance(
+            'array',
+            [
+                'content' => $messages,
+            ]
+        );
+    }
+}
